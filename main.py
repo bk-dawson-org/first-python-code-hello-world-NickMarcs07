02ec38f87c9ef88b37cbad4dfa70cdc231b88594
NicholasMarcogliese <nicholas.marcogliese@dawsoncollege.qc.ca>
2024-09-10 20:09:48 +0000

    quiz2

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+fahrenheit = float(input("Input a temperature in degrees Fahrenheit"))
+celsius = (fahrenheit - 32)* 5 / 9
+print(' That \'s' , celsius , " degrees celsius !")            
+num = 9 ** (1/2)
+print(num)
+x = 97 + 2 / 12 - 18*3 % 2
+print(x)
+
+
+
+
+
+
 
---
+++ quiz.py
@@ -0,0 +1,5 @@
+r = float(input("Input a number for radius"))
+π = 3.14
+Volume = float((4/3) * π * (r**3))
+print(Volume)
+
